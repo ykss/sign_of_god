d9982b6669ac614182c89a0926005145e6002322
ykss <yukyeongsang@gmail.com>
2021-11-10 13:36:51 +0900

    feat : Kakao map 기능 추가

---
+++ react_invitation/src/Components/KakaoMaps.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import config from "../config";
+
+const { kakao } = window;
+
+function KakaoMaps() {
+  useEffect(() => {
+    const container = document.getElementById("myMap");
+    const options = {
+      center: new kakao.maps.LatLng(
+        config.place.latitude,
+        config.place.longitude
+      ),
+      level: 2,
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    // 마커가 표시될 위치입니다
+    var markerPosition = new kakao.maps.LatLng(
+      config.place.latitude,
+      config.place.longitude
+    );
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    var iwContent = `<span style="    
+    display: block;
+    color:black;
+    text-align: center;
+    height: 24px;
+    line-height:22px;
+    border-radius:4px;
+    padding:5px 10px;">${config.place.address}</span>`;
+    var iwPosition = new kakao.maps.LatLng(
+      config.place.latitude,
+      config.place.longitude
+    ); //인포윈도우 표시 위치입니다
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+      position: iwPosition,
+      content: iwContent,
+    });
+
+    // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+    infowindow.open(map, marker);
+  }, []);
+
+  return (
+    <div
+      id="myMap"
+      style={{
+        width: "430px",
+        height: "430px",
+      }}
+    ></div>
+  );
+}
+
+export default KakaoMaps;
--- react_invitation/src/Pages/Map.jsx
+++ react_invitation/src/Pages/Map.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import GoogleMaps from '../Components/GoogleMaps'
+import React from "react";
+import styled from "styled-components";
+import KakaoMaps from "../Components/KakaoMaps";
+import config from "../config";
+
+const MapInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 
 function Map() {
-    return (
-        <GoogleMaps/>
-    )
+  return (
+    <>
+      <MapInfoWrapper>
+        <h2>오시는길</h2>
+        <p>{config.place.name}</p>
+        <p>{config.place.address}</p>
+        <p>문의 : {config.place.contact}</p>
+      </MapInfoWrapper>
+      <KakaoMaps />
+    </>
+  );
 }
 
-export default Map
+export default Map;
