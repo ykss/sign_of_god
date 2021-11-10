30912e2b5559de4ff273121895a5969e3dd63d7c
ykss <yukyeongsang@gmail.com>
2021-11-10 13:37:02 +0900

    feat : 설정 파일 추가

---
+++ react_invitation/src/config.js
@@ -0,0 +1,19 @@
+const config = {
+    global: {
+      kakaomapAPIKey: '20917a68770a34db8e253ee0f1ea3cd8', 
+    },
+    title: `'하나님의 사인' 출판 기념회`, 
+    author:{
+      name: '유영창'
+    },
+    place: {
+      name: '목천교회 본당 (2층)',
+      address: '서울 중랑구 용마산로 432 (면목동, 목천교회)',
+      contact:'02-434-0153',
+      latitude: 37.589756,
+      longitude: 127.097318,
+    },
+    date : '11월 27일 토요일 3시'
+  };
+
+  export default config;
