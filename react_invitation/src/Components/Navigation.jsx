9fcc8c37da22a1b8418613cb763e4222d72fa400
ykss <yukyeongsang@gmail.com>
2021-11-11 14:10:25 +0900

    feat : 네이버길찾기 추가

--- react_invitation/src/Components/Navigation.jsx
+++ react_invitation/src/Components/Navigation.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import config from "../config";
+import tmap from "../Images/tmap.png";
+import kakao_map from "../Images/kakaomap.png";
+import naver_map from "../Images/naver-map.png";
 
 const NavigationWrapper = styled.div`
   top: 0px;
@@ -52,7 +55,7 @@ function Navigation() {
             <a
               href={`https://apis.openapi.sk.com/tmap/app/routes?appKey=${config.global.tmapAPIKey}&name=${config.place.name}&lon=${config.place.longitude}&lat=${config.place.latitude}`}
             >
-              <ImageIcon src="./tmap.png" alt="tmap" />
+              <ImageIcon src={tmap} alt="tmap" />
               <br />
             </a>
             <IconLabel>티맵</IconLabel>
@@ -61,10 +64,19 @@ function Navigation() {
             <a
               href={`https://map.kakao.com/link/map/${config.place.name},${config.place.latitude},${config.place.longitude}`}
             >
-              <ImageIcon src="./kakaomap.png" alt="kakaomap" />
+              <ImageIcon src={kakao_map} alt="kakaomap" />
             </a>
             <br />
             <IconLabel>카카오맵</IconLabel>
+          </TableData>
+          <TableData>
+            <a
+              href={`http://m.map.naver.com/route.nhn?menu=route&ename=${config.place.name}&ex=${config.place.longitude}&ey=${config.place.latitude}&pathType=0&showMap=true`}
+            >
+              <ImageIcon src={naver_map} alt="navermap" />
+            </a>
+            <br />
+            <IconLabel>네이버지도</IconLabel>
           </TableData>
         </tr>
       </NaviTable>
