163bd980d90b1b17f2f3b20ed7a03ef5a23b4277
ykss <yukyeongsang@gmail.com>
2021-11-16 14:12:16 +0900

    fix : config 날짜 수정

--- react_invitation/src/config.js
+++ react_invitation/src/config.js
@@ -1,23 +1,23 @@
 import { KAKAO_MAP_API_KEY, T_MAP_API_KEY } from "./API_KEY";
 
 const config = {
-    global: {
-      kakaomapAPIKey: KAKAO_MAP_API_KEY,
-      tmapAPIKey: T_MAP_API_KEY 
-    },
-    title: `유영창 장로의 출판 기념회`, 
-    book:`'하나님의 사인(Sign of GOD)'`,
-    author:{
-      name: '유영창'
-    },
-    place: {
-      name: '목천교회 본당 (2층)',
-      address: '서울 중랑구 용마산로 432(목천교회)',
-      contact:'02-434-0153',
-      latitude: 37.589756,
-      longitude: 127.097318,
-    },
-    date : '11월 27일 토요일 3시'
-  };
+  global: {
+    kakaomapAPIKey: KAKAO_MAP_API_KEY,
+    tmapAPIKey: T_MAP_API_KEY,
+  },
+  title: `유영창 장로의 출판 기념회`,
+  book: `'하나님의 사인(Sign of GOD)'`,
+  author: {
+    name: "유영창",
+  },
+  place: {
+    name: "목천교회 본당 (2층)",
+    address: "서울 중랑구 용마산로 432(목천교회)",
+    contact: "02-434-0153",
+    latitude: 37.589756,
+    longitude: 127.097318,
+  },
+  date: "12월 18일 토요일 3시",
+};
 
-  export default config;
+export default config;
